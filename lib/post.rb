c9596b773f221a9c7169156214a7f3bf1d0396c5
Alice Freda <alicefreda@gmail.com>
2019-11-04 02:17:14 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+  attr_accessor :author, :title
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  attr_accessor :artist, :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    save
+  end
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end
